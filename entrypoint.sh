015f68d338d233e7caa5a1489ca8d7d7e459789f
Iacopof <iacopo.filiberto@manydesigns.com>
2020-11-12 20:09:16 +0100

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
-#/bin/sh -l
+#!/bin/sh -l
 POM_PATH="${1-.}"
 POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
 
+echo "Founded version ${POM_VERSION}"
 echo ::set-output name=version::$POM_VERSION
