a805c0dcbcf3657ce6f6f5434d0ae22cae37536d
Richard PERES <43060465+PERES-Richard@users.noreply.github.com>
2023-06-12 21:21:17 +0200

    Use GITHUB_OUTPUT instead of depreciated 'set-output'

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ POM_PATH="${1-.}"
 POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 echo "Founded version ${POM_VERSION}"
-echo ::set-output name=version::$POM_VERSION
+echo "POM_VERSION=$POM_VERSION" >> $GITHUB_OUTPUT
